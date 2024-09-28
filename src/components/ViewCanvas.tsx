c670deba70a9261e5b188fc331a351c271687618
Yannik Sahl <me@yanniksahl.com>
2024-09-28 15:37:59 +0200

    fix prismic simulator

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -1,36 +1,44 @@
 "use client";
 
-import Scene from "@/slices/Hero/Scene";
+import dynamic from "next/dynamic";
+
 import { View } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+const Loader = dynamic(
+  () => import("@react-three/drei").then((mod) => mod.Loader),
+  { ssr: false },
+);
 
 interface ViewCanvasProps {}
 
 const ViewCanvas = ({}: ViewCanvasProps) => {
   return (
-    <Canvas
-      style={{
-        position: "fixed",
-        top: 0,
-        left: "50%",
-        transform: "translateX(-50%)",
-        overflow: "hidden",
-        pointerEvents: "none",
-        zIndex: 30,
-      }}
-      camera={{ fov: 30 }}
-      shadows
-      dpr={[1, 1.5]}
-      gl={{ antialias: true }}
-    >
-      <mesh rotation={[0.5, 0.5, 0]} position={[1, 0, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color={"hotpink"} />
-      </mesh>
-      <Scene />
-      <View.Port />
-      {/* <Perf /> */}
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          overflow: "hidden",
+          pointerEvents: "none",
+          zIndex: 30,
+        }}
+        camera={{ fov: 30 }}
+        shadows
+        dpr={[1, 1.5]}
+        gl={{ antialias: true }}
+      >
+        {/* 
+        <Scene /> */}
+        <Suspense fallback={null}>
+          <View.Port />
+        </Suspense>
+        {/* <Perf /> */}
+      </Canvas>
+      <Loader />
+    </>
   );
 };
 
